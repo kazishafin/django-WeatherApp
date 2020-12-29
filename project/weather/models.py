81da27cd634b9f2bc471f41def0b2d9589b6ab70
Kazi Shafin <kazishafin1@gmail.com>
2020-12-29 11:01:12 +0600

    ..

--- project/weather/models.py
+++ project/weather/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=25, unique=True)
-    order_date = models.DateField(null=True)
     
     def __str__(self):
         return self.name 
